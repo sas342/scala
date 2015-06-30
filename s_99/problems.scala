77a3c9f9c5ba3f4b5e06dd0f3aa9642d389d50ba
= <=>
2015-06-29 21:46:20 -0400

    problem 9

--- s_99/problems.scala
+++ s_99/problems.scala
@@ -47,15 +47,24 @@ def flatten(l: List[Any]): List[Any] = l flatMap {
 	
 }
 
+
 //problem 8: Eliminate consecutive duplicates of list elements.
-def compress(l: List[Any]): List[Any] = {
-	val f= (a:Any, b:List[Any]) => if (b.length == 0 || a != b.head) a :: b else b
-	foldRight(l, Nil:List[Any])(f)
+def compress[A](l: List[A]): List[A] = {
+	val f= (a:A, b:List[A]) => if (b.length == 0 || a != b.head) a :: b else b
+	foldRight(l, Nil:List[A])(f)
 }
 
+
+//problem 9: pack consecutive duplicates into sublists
+def pack[A](list: List[A]):List[List[A]] = 
+	foldRight(list, Nil:List[List[A]]){(a:A,b:List[List[A]]) => if (b.length == 0) List[A](a) :: b else if (b.head.head == a) {val t = a :: b.head; t :: (b.drop(1))} else List[A](a) :: b}
+
+//problem 9a: improved with using list span method
+//def pack_improved[A]
 
 println("reverse of (1,2,3,4) is "+reverse(List(1,2,3,4)))
 println("isPalindrome (1,2,3) is "+isPalindrome(List(1,2,3)))
 println("isPalindrome (1,2,1) is "+isPalindrome(List(1,2,1)))
 println("flatMap(List(List(1, 1), 2, List(3, List(5, 8)))) is " + flatten(List(List(1,1),2,List(3,List(5,8)))))
 println("removing dups of compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) "+compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+println("pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) = "+pack(List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')))
